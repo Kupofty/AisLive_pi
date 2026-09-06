d9c8681b50a692554b03801075752863fddd3895
Kupofty <69522980+Kupofty@users.noreply.github.com>
2026-09-06 16:19:24 +0200

    add websocket connection to ais.openwater.io stream

--- src/main-ui/main_ui_derived.h
+++ src/main-ui/main_ui_derived.h
@@ -1,6 +1,10 @@
 #ifndef DIALOG_MAIN_GUI
 #define DIALOG_MAIN_GUI
 
+#include <atomic>
+#include <memory>
+#include <thread>
+
 #include "main_ui_base.h"
 #include "settings/global_settings.h"
 
@@ -9,15 +13,28 @@ class Plugin;
 // Main class
 class DialogMainGui : public DialogMainGuiBase
 {
-  public:
+public:
     DialogMainGui(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("AisLive Plugin GUI"),
                   const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_DIALOG_STYLE);
     ~DialogMainGui();
 
     Plugin* plugin = nullptr;
 
-  protected:
+protected:
     void OnClose(wxCloseEvent& event) override;
+    void OnButtonClick_startStream(wxCommandEvent& event) override;
+    void OnButtonClick_stopStream(wxCommandEvent& event) override;
+
+private:
+    struct AisStreamSession;
+    std::unique_ptr<AisStreamSession> m_aisSession;
+
+    std::thread m_streamThread;
+    std::atomic<bool> m_streaming{false};
+
+    void StartAisStream();
+    void StopAisStream();
+    void AisStreamThreadFunc();
 };
 
 #endif //DIALOG_MAIN_GUI
